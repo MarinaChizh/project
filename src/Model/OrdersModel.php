a71379b8aad82ef98f5fca64f802e1f72c51e8e8
MarinaChizh <47003040+MarinaChizh@users.noreply.github.com>
2019-11-01 16:20:01 +0300

    01/11/2019

--- src/Model/OrdersModel.php
+++ src/Model/OrdersModel.php
@@ -11,7 +11,8 @@ class OrdersModel extends DbEntity
     {
         $this
         ->reset()
-        ->setSelect("`orders`.`id`, `orders`.`name`, `orders`.`unp`, `orders`.`route `, `orders`.`volume`, `orders`.`download date`, `orders`.`date of discharge`, `orders`.`price`  AS 'cars_id'")
+        ->setSelect("`orders`.`id`, `orders`.`name`, `orders`.`unp`, `orders`.`route`, `orders`.`volume`, `orders`.`download_date`, 
+        `orders`.`date_of_discharge`, `orders`.`price`, `cars`.`number` AS `cars_id`")
         ->setFrom("`cars`, `orders`")
         ->setWhere("`cars`.`id` = `orders`.`cars_id`")
         ->setOrderBy("`orders`.`id`");
@@ -20,3 +21,4 @@ class OrdersModel extends DbEntity
 
 
 }
+
