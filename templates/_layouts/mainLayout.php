05b09282ce8aacbc0fb5633e1f48a42ec72b996a
MarinaChizh <47003040+MarinaChizh@users.noreply.github.com>
2019-11-22 17:00:56 +0300

    22/11/2019

--- templates/_layouts/mainLayout.php
+++ templates/_layouts/mainLayout.php
@@ -16,69 +16,64 @@ use App\View\Helper\HTML;
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?= $title ?></title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="public/css/style.css">
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
 
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="<?= Dispatcher::dispatcher()->encodeUri("site/home") ?>">TransCompany</a>
+  <nav class="top-menu">
+    
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="<?= Dispatcher::dispatcher()->encodeUri("site/home") ?>">Главная <span class="sr-only">(current)</span></a>
+    <div>
+      <ul class="menu-main">
+        <li class="left-item">
+          <a href="<?= Dispatcher::dispatcher()->encodeUri("site/home") ?>">Главная</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= Dispatcher::dispatcher()->encodeUri("site/about") ?>">О нас</a>
+        <li class="left-item">
+          <a href="<?= Dispatcher::dispatcher()->encodeUri("site/about") ?>">О нас</a>
         </li>
-        <li class="nav-item">
-          <a class='nav-link' href='<?= Dispatcher::dispatcher()->encodeUri('cars/show', ['page' => 1]) ?>'>Автомобили</a>
+        <li class="left-item">
+          <a href='<?= Dispatcher::dispatcher()->encodeUri('cars/show', ['page' => 1]) ?>'>Автомобили</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= Dispatcher::dispatcher()->encodeUri("orders/show", ['page' => 1]) ?>">Заказы</a>
+        <li class="left-item">
+          <a href="<?= Dispatcher::dispatcher()->encodeUri("orders/show", ['page' => 1]) ?>">Заказы</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= Dispatcher::dispatcher()->encodeUri("site/loginform") ?>">Войти на сайт</a>
+        <li class="right-item">
+          <a href="<?= Dispatcher::dispatcher()->encodeUri("site/loginform") ?>">Войти на сайт</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= Dispatcher::dispatcher()->encodeUri("signup/showform") ?>">Регистрация</a>
+        <li class="right-item">
+          <a href="<?= Dispatcher::dispatcher()->encodeUri("signup/showform") ?>">Регистрация</a>
         </li>
         <?php if ($deleteEditAccess) {
           echo"
-        <li class='nav-item'>
-        <a class='nav-link' href='<?= Dispatcher::dispatcher()->encodeUri('usergroup/show', ['page' => 1]) ?>'>User_group</a>
+        <li class='right-item'>
+        <a href='<?= Dispatcher::dispatcher()->encodeUri('usergroup/show', ['page' => 1]) ?>'>User_group</a>
         </li>
-        <li class='nav-item'>
-        <a class='nav-link' href='<?= Dispatcher::dispatcher()->encodeUri('users/show', ['page' => 1]) ?>'>User</a> 
+        <li>
+        <a href='<?= Dispatcher::dispatcher()->encodeUri('users/show', ['page' => 1]) ?>'>User</a> 
         </li>";
         }
         ?>
-        
       </ul>
+      <a class="navbar-logo" href="<?= Dispatcher::dispatcher()->encodeUri("site/home") ?>">
+      <img src='public/css/123.png' width="300" height="220">
+    </a>
     </div>
   </nav>
-  <!-- <div class="container"> -->
-
-  <div id="user_state" class="float-right">
+  
+  <div>
     <?= Auth::currentUserInfo() . " " . (isset($_SESSION['user']) ? "<a href='?a=logout'>Выход</a>" : "") ?>
   </div>
   <br />
-  <!-- </div> -->
+  
 
-  <div id="maincontent">
+  <div>
     <?php $this->body(); ?>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 
